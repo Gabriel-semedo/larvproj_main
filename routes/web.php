2511fe92526654cf698c4922a748d0de12e6d1dc
Mariana Tavares <mariana.tavares@proside.pt>
2025-04-03 12:51:14 +0100

    feat: visit routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 
+use App\Http\Controllers\VisitsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/visits', VisitsController::class);
 
